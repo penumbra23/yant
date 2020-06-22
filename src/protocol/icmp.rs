fbe11e7af66cf54e02a049c9feaed39afd1355f6
penumbra23 <glbranimir@gmail.com>
2020-06-22 23:12:23 +0200

    Add ICMP support for Linux

--- src/protocol/icmp.rs
+++ src/protocol/icmp.rs
@@ -1,8 +1,43 @@
 use clap::ArgMatches;
 use std::str;
 
+#[cfg(target_os = "linux")]
+use fastping_rs::Pinger;
+#[cfg(target_os = "linux")]
+use fastping_rs::PingResult::{Idle, Receive};
+
+#[cfg(target_os = "windows")]
 use winping::{Buffer, Pinger};
 
+#[cfg(target_os = "linux")]
+pub fn handle_icmp(matches: &ArgMatches) {
+    let server = matches.value_of("target").unwrap();
+    let count = matches.value_of("count").unwrap().parse::<i32>().unwrap();
+
+    let (pinger, results) = match Pinger::new(None, None) {
+        Ok((pinger, results)) => (pinger, results),
+        Err(e) => panic!("Error creating pinger: {}", e)
+    };
+
+    pinger.add_ipaddr(server);
+    
+    println!("Pinging address {}, request number {}:", server, count);
+
+    for _ in 0..count {
+        pinger.ping_once();
+        match results.recv() {
+            Ok(result) => {
+                match result {
+                    Idle{addr} => { /*DO NOTHING*/ },
+                    Receive{addr, rtt} => println!("Response time: {} ms.", rtt.as_millis() as u64),
+                }
+            },
+            Err(e) => panic!("{}", e),
+        }
+    }
+}
+
+#[cfg(target_os = "windows")]
 pub fn handle_icmp(matches: &ArgMatches) {
     let server = matches.value_of("target").unwrap();
     let count = matches.value_of("count").unwrap().parse::<i32>().unwrap();
